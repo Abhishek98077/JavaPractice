124b832a9ce649520c6a575ffd5c3302f1ea69ff
Abhishek <rishut195@gmail.com>
2024-06-20 16:30:12 +0530

    my Changes

---
+++ src/JunePracticals/lab001.java
@@ -0,0 +1,9 @@
+package JunePracticals;
+
+public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("Jai mata di");
+        System.out.println("Jai bajrang bali");
+        System.out.println("jai shree ram");
+    }
+}
