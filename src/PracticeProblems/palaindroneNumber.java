44c93547349b1f12b7340907cb862ca5428ea27a
Abhishek <rishut195@gmail.com>
2024-06-21 15:51:03 +0530

    my changes

--- src/PracticeProblems/palaindroneNumber.java
+++ src/PracticeProblems/palaindroneNumber.java
@@ -7,7 +7,6 @@ import static java.lang.Integer.reverse;
 public class palaindroneNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number : ");
         int number = sc.nextInt();
         int temp  , sum=0 , remainder;
         temp = number;
