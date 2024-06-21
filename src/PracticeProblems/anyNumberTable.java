f396ad9938ba5b6cf1e9959c1ff1112fc7ea4ede
Abhishek <rishut195@gmail.com>
2024-06-21 14:07:24 +0530

    my changes

--- src/PracticeProblems/anyNumberTable.java
+++ src/PracticeProblems/anyNumberTable.java
@@ -1,2 +1,18 @@
-package PracticeProblems;public class anyNumberTable {
+package PracticeProblems;
+
+import java.util.Scanner;
+
+public class anyNumberTable {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int number = sc.nextInt();
+        System.out.println("Enter the multiples");
+        int numberTimes = sc.nextInt();
+
+        for (int i = 0; i <= numberTimes; i++) {
+            System.out.println(number + "*" + i + "="+ number * i );
+        }
+
+    }
 }
--- src/PracticeProblems/palaindroneNumber.java
+++ src/PracticeProblems/palaindroneNumber.java
@@ -1,2 +1,27 @@
-package PracticeProblems;public class palaindroneNumber {
+package PracticeProblems;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.reverse;
+
+public class palaindroneNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int number = sc.nextInt();
+        int temp  , sum=0 , remainder;
+        temp = number;
+        while (number < 0) {
+            remainder = number % 10;
+            number = number / 10;
+            sum = (sum * 10) + remainder;
+            number = number %10;
+        }
+        if(sum == number){
+            System.out.println("The number is palaindrone");
+        }else{
+            System.out.println("The number is not palaindrone");
+        }
+
+    }
 }
